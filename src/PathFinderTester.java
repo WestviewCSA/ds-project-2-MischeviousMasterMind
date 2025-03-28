67b509ac24b7ee4469a2b541f6a62f9590da1b0d
MischeviousMasterMind <0KAY04557@gmail.com>
2025-03-28 10:07:37 -0700

    Finished tests for test01

--- src/PathFinderTester.java
+++ src/PathFinderTester.java
@@ -1,3 +1,4 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
@@ -7,60 +8,61 @@ import org.junit.jupiter.api.Test;
 
 class PathFinderTester {
 
-	@Test
-	public void test01() throws FileNotFoundException, IncompleteMapException, IncorrectMapFormatException,
-			IllegalMapCharacterException {
-
-//		assertTrue(test("TEST/test01/test01", "TEST/test01/test01-queue", Approach.QUEUE, Maze.TEXT_BASED));
-		assertTrue(test("TEST/test01/test01", "TEST/test01/test01-stack", Approach.STACK, Maze.TEXT_BASED));
-		assertTrue(test("TEST/test01/test01", "TEST/test01/test01-queue", Approach.OPT, Maze.TEXT_BASED));
-
-	}
-
-	private static boolean test(String inputFile, String solutionFile, Approach approach, boolean format)
-			throws FileNotFoundException, IncompleteMapException, IncorrectMapFormatException,
-			IllegalMapCharacterException {
-
-		char[][][] output = Maze.read(new File(inputFile), format);
-		char[][][] solution = Maze.read(new File(solutionFile), format, true);
-
-		System.out.println("Input: ");
-		Maze.print(output, System.out, format);
-
-		long startTime = System.nanoTime();
-		PathFinder.solve(output, approach);
-		long endTime = System.nanoTime();
+    @Test
+    public void test01() throws FileNotFoundException, IncompleteMapException, IncorrectMapFormatException,
+            IllegalMapCharacterException {
 		
-		System.out.println("\nOutput: ");
-		Maze.print(output, System.out, format);
+		assertTrue(test("TEST/test01/test01", "TEST/test01/test01-queue", Approach.QUEUE, Maze.TEXT_BASED));
+        assertTrue(test("TEST/test01/test01", "TEST/test01/test01-stack", Approach.STACK, Maze.TEXT_BASED));
+        assertTrue(test("TEST/test01/test01", "TEST/test01/test01-opt", Approach.OPT, Maze.TEXT_BASED));
 
-		System.out.println("\nSolution: ");
-		Maze.print(solution, System.out, format);
+    }
 
-		Maze.secretFeature(output, System.out);
-		
-		System.out.print("\nTotal time to solve: " + (endTime - startTime) / 1_000_000_000.0 + " seconds");
+    private static boolean test(String inputFile, String solutionFile, Approach approach, boolean format)
+            throws FileNotFoundException, IncompleteMapException, IncorrectMapFormatException,
+            IllegalMapCharacterException {
 
-		return equals(output, solution);
-	}
+        char[][][] output = Maze.read(new File(inputFile), format);
+        char[][][] solution = Maze.read(new File(solutionFile), format, true);
 
-	private static boolean equals(char[][][] array1, char[][][] array2) {
+        System.out.println("Input: ");
+        Maze.print(output, System.out, format);
 
-		for (int i = 0; i < array1.length; i++) {
+        long startTime = System.nanoTime();
+        PathFinder.solve(output, approach);
+        long endTime = System.nanoTime();
 
-			for (int ii = 0; ii < array1[i].length; ii++) {
+        System.out.println("\nOutput: ");
+        Maze.print(output, System.out, format);
 
-				for (int iii = 0; iii < array1[i][ii].length; iii++) {
- 
-					if (array1[i][ii][iii] != array2[i][ii][ii])
-						return false;
-				}
+        System.out.println("\nSolution: ");
+        Maze.print(solution, System.out, format);
 
-			}
+        Maze.secretFeature(output, System.out);
 
-		}
+        System.out.print("\nTotal time to solve: " + (endTime - startTime) / 1_000_000_000.0 + " seconds");
 
-		return true;
-	}
+        return equals(output, solution);
+    }
+
+    private static boolean equals(char[][][] array1, char[][][] array2) {
+
+        for (int i = 0; i < array1.length; i++) {
+
+            for (int ii = 0; ii < array1[i].length; ii++) {
+
+                for (int iii = 0; iii < array1[i][ii].length; iii++) {
+
+                    if (array1[i][ii][iii] != array2[i][ii][iii]) {
+                        return false;
+                    }
+                }
+
+            }
+
+        }
+
+        return true;
+    }
 
 }
